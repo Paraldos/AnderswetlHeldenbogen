d05af0bedc301deda7ff42554c23163f7563ceb6
Paraldos <hillbrecht.s@gmail.com>
2023-11-25 09:28:40 +0100

    WIP Grundlagen nameElement added

--- js/grundlagen/grundlagen.js
+++ js/grundlagen/grundlagen.js
@@ -1,15 +1,9 @@
 import Section from "../section/section.js";
+import NameElement from "./nameElement.js";
 
 export default class Grundlagen {
   constructor() {
     this.section = new Section("Grundlagen");
-    this.addName();
-  }
-
-  addName() {
-    let name = document.createElement("li");
-    name.classList.add("grundlagen__name");
-    name.innerHTML = "Hello World";
-    this.section.contentContainer.appendChild(name);
+    this.nameElement = new NameElement(this.section);
   }
 }
