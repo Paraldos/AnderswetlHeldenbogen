696562de0ca5bf6eecf3fbbc1c87ada9aa9fb80a
Sieghard Hillbrecht <s.hillbrecht@techdivision.com>
2023-12-13 15:12:59 +0100

    Renamed navbar folder

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 import db from "./data/db.js";
 import hero from "./data/hero.js";
 import Main from "./components/main/main.js";
-import Navbar from "../components/menus/navbar.js";
-import HeroMenu from "../components/menus/heroMenu.js";
+import Navbar from "../components/navbar/navbar.js";
+import HeroMenu from "../components/navbar/heroMenu.js";
 
 new Main();
 new Navbar();
