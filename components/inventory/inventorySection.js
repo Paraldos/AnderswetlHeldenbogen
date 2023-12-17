a72a1423177b46238b9731c07147c1e1cf8ebc70
Paraldos <hillbrecht.s@gmail.com>
2023-12-17 20:07:43 +0100

    put consumables into their own file

--- components/inventory/inventorySection.js
+++ components/inventory/inventorySection.js
@@ -1,6 +1,7 @@
 import db from "../../data/db.js";
 import hero from "../../data/hero.js";
 import Section from "../section/section.js";
+import Consumable from "./consumable.js";
 
 export default class InventorySection {
   constructor() {
@@ -34,61 +35,5 @@ export default class InventorySection {
     hero.consumables.forEach((consumable, index) => {
       new Consumable(consumable, index, this.consuamblesContainer);
     });
-  }
-}
-
-class Consumable {
-  constructor(consumable, index, container) {
-    this.consumable = consumable;
-    this.index = index;
-    this.container = container;
-    this.element = this.initElement();
-
-    this.value = this.element.querySelector(".consumable__value");
-
-    this.name = this.element.querySelector(".consumable__name");
-    this.name.addEventListener("change", () => this.onNameChange());
-
-    this.plsBtn = this.element.querySelector(".consumable__plus-btn");
-    this.plsBtn.addEventListener("click", () => this.onPlsBtnClick());
-
-    this.minusBtn = this.element.querySelector(".consumable__minus-btn");
-    this.minusBtn.addEventListener("click", () => this.onMinusBtnClick());
-  }
-
-  // ================== init
-  initElement() {
-    let element = Object.assign(document.createElement("div"), {
-      className: "consumable",
-      innerHTML: `
-        <input class="consumable__name" type="text" value="${this.consumable.name}" />
-        <p class="consumable__value">${this.consumable.value}</p>
-        <button class="consumable__minus-btn symbol-btn"><i class="fa-solid fa-minus"></i></button>
-        <button class="consumable__plus-btn symbol-btn"><i class="fa-solid fa-plus"></i></button>`,
-    });
-    this.container.appendChild(element);
-    return element;
-  }
-
-  // ================== events
-  onNameChange() {
-    this.consumable.name = this.name.value;
-    hero.saveHero();
-  }
-
-  onPlsBtnClick() {
-    this.consumable.value++;
-    this.value.innerHTML = this.consumable.value;
-    hero.saveHero();
-  }
-
-  onMinusBtnClick() {
-    this.consumable.value--;
-    this.value.innerHTML = this.consumable.value;
-    if (this.consumable.value < 0) {
-      hero.consumables.splice(this.index, 1);
-      this.element.remove();
-    }
-    hero.saveHero();
   }
 }
