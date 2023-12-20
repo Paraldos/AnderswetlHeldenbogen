c135a6ec4d03e328df06d6bd79bcec2a1f2bb5da
Paraldos <hillbrecht.s@gmail.com>
2023-12-20 20:04:31 +0100

    minor changes to navbar

--- components/navbar/navbar.js
+++ components/navbar/navbar.js
@@ -3,6 +3,9 @@ import HerosModal from "./herosModal.js";
 export default class Navbar {
   constructor() {
     this.heroBtn = document.querySelector(".navbar__hero-btn");
+    this.wikiBtn = document.querySelector(".navbar__wiki-btn");
+    this.editBtn = document.querySelector(".navbar__edit-btn");
+    // events
     this.heroBtn.addEventListener("click", () => new HerosModal());
   }
 }
