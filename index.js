b77f830db5eadedb02936eb0fc7697d9de183dca
Paraldos <hillbrecht.s@gmail.com>
2024-01-24 20:22:47 +0100

    local instance with xampp

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import db from "./data/db.js";
 import hero from "./data/hero.js";
 import Main from "./components/main/main.js";
-import Navbar from "../components/navbar/navbar.js";
+import Navbar from "./components/navbar/navbar.js";
 
 new Main();
 new Navbar();
